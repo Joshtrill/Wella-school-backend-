ea2f23cb1dd5c4fc0d98170efeae9e87b728f676
Joshua <moyosoreolabisi07@gmail.com>
2022-04-07 00:56:21 +0100

    week1 assignment

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Joshua Akinola, I am in the beginner class at Wella Schools");
+            Console.WriteLine("My reasons for learning the backend program is very simple. i have always wanted to get my hand on coding but i haven't been finding the right motivation not until of recent. it was at this same time I saw the advert of Wella Schools. i really want to know what happens behind the apps i use daily nd probably bring my ideas to life.");
+        }
+    }
+}
