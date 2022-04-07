57bbbf00be001dee6fef52ef9cf4c6bfb38daa36
Joshua <moyosoreolabisi07@gmail.com>
2022-04-07 10:58:31 +0100

    weekq lab1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace week1
     class Program
     {
         static void Main(string[] args)
+        // Lab 1 Week1
         {
             Console.WriteLine("Joshua Akinola, I am in the beginner class at Wella Schools");
             Console.WriteLine("My reasons for learning the backend program is very simple. i have always wanted to get my hand on coding but i haven't been finding the right motivation not until of recent. it was at this same time I saw the advert of Wella Schools. i really want to know what happens behind the apps i use daily nd probably bring my ideas to life.");
